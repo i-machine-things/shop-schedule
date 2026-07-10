4846a3b6f59de5f58a0d337a7d070f353958439e
Allan Noble <allann2020@gmail.com>
2026-07-10 09:38:10 -0700

    perf: increase client kiosk scroll cap from 20fps to 30fps

--- install-client.sh
+++ install-client.sh
@@ -58,7 +58,7 @@ exec /usr/bin/chromium \\
   --disable-gpu-shader-disk-cache \\
   --disable-dev-shm-usage \\
   --disable-gpu-rasterization \\
-  "$SERVER_URL/kiosk.html?fps=20"
+  "$SERVER_URL/kiosk.html?fps=30"
 XINITRC
 chmod +x "$USER_HOME/.xinitrc"
 
